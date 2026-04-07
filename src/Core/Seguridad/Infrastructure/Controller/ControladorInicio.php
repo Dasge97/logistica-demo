599e39b9e55bfbdbedec7ffb801933bea0c266c2
dasge <dasge97@gmail.com>
2026-04-07 19:55:06 +0000

    Mejorar dashboard operativo del backoffice

--- src/Core/Seguridad/Infrastructure/Controller/ControladorInicio.php
+++ src/Core/Seguridad/Infrastructure/Controller/ControladorInicio.php
@@ -2,6 +2,11 @@
 
 namespace App\Core\Seguridad\Infrastructure\Controller;
 
+use App\Modulos\Catalogos\Infrastructure\Persistence\Doctrine\RepositorioNivelServicioEntrega;
+use App\Modulos\Catalogos\Infrastructure\Persistence\Doctrine\RepositorioTipoCliente;
+use App\Modulos\Catalogos\Infrastructure\Persistence\Doctrine\RepositorioTipoVehiculo;
+use App\Modulos\Decisiones\Infrastructure\Persistence\Doctrine\RepositorioSnapshotTarificacionPedido;
+use App\Modulos\Pedidos\Infrastructure\Persistence\Doctrine\RepositorioPedido;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +14,24 @@ use Symfony\Component\Routing\Attribute\Route;
 final class ControladorInicio extends AbstractController
 {
     #[Route('/', name: 'app_inicio', methods: ['GET'])]
-    public function __invoke(): Response
+    public function __invoke(
+        RepositorioPedido $repositorioPedido,
+        RepositorioSnapshotTarificacionPedido $repositorioSnapshotTarificacionPedido,
+        RepositorioTipoCliente $repositorioTipoCliente,
+        RepositorioNivelServicioEntrega $repositorioNivelServicioEntrega,
+        RepositorioTipoVehiculo $repositorioTipoVehiculo,
+    ): Response
     {
-        return $this->render('inicio/index.html.twig');
+        return $this->render('inicio/index.html.twig', [
+            'metricas' => [
+                'pedidos' => $repositorioPedido->count([]),
+                'snapshots' => $repositorioSnapshotTarificacionPedido->count([]),
+                'tiposCliente' => $repositorioTipoCliente->count([]),
+                'nivelesServicio' => $repositorioNivelServicioEntrega->count([]),
+                'tiposVehiculo' => $repositorioTipoVehiculo->count([]),
+            ],
+            'ultimosPedidos' => $repositorioPedido->findBy([], ['createdAt' => 'DESC'], 5),
+            'ultimosSnapshots' => $repositorioSnapshotTarificacionPedido->findBy([], ['createdAt' => 'DESC'], 5),
+        ]);
     }
 }
